9f59fc3b53d20c12ffca15c512e10c512e96e955
Rodrigo Birriel <rbirriel@altimetrik.com>
2015-02-25 10:21:59 -0200

    commit with first code

---
+++ testingmodules/testingmodules/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testingmodules
+//
+//  Created by Rodrigo Birriel on 2/25/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
